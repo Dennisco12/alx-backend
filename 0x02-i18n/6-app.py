973182340dc3666297b0df6b392fa153517f8aa7
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-02-28 20:37:43 -0800

    Added tasks 6 to 7

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with internationalization support.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+from typing import Dict, Union
+
+
+class Config:
+    """A babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Detect the locale"""
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """The entry point"""
+    return render_template('6-index.html')
+
+
+users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    }
+
+def get_user() -> Union[Dict, None]:
+    """This returns a user dict"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """This sets the current user as a global on flask.g.user"""
+    currentUser = get_user()
+    g.user = currentUser
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
